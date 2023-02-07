6dc4426a48fa47861d7666c23837dcd6fb6c58b5
Monir Ullah <mullah725@gmail.com>
2023-02-08 01:08:06 +0600

    SQL

--- new-plugins/uninstall.php
+++ new-plugins/uninstall.php
@@ -15,4 +15,13 @@
     foreach($books as  $book){
         wp_delete_post( $book->ID, true );
     }
+
+
+    // accessing database via SQL
+
+    global $wpdb;
+    $wpdb->query("DELETE FROM  wp_posts WHERE post_type = 'book'");
+    $wpdb->query("DELETE FROM  wp_postmeta WHERE post_id NOT IN (SELECT id FROM wp_posts)");
+    $wpdb->query("DELETE FROM  wp_term_relatationships WHERE object_id NOT IN (SELECT id FROM wp_posts)");
+
 ?>

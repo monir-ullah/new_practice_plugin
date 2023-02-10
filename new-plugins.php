c47b368b92cf5130e8b5e5da9b88b904692679ee
Monir Ullah <mullah725@gmail.com>
2023-02-11 00:57:48 +0600

    admin page

--- new-plugins.php
+++ new-plugins.php
@@ -26,9 +26,12 @@
     defined('ABSPATH') or die('Hey, What are your doing here. You silly human');
 
     class AlecadddPlugin{
-      
+        
+        public $plugin_link;
         function __construct(){
             add_action('init', [$this, 'custom_post_type']);
+
+            $this->plugin_link = plugin_basename( __FILE__ );
         }
         function activation(){
             $this->custom_post_type();
@@ -36,6 +39,22 @@
         }
         function register(){
             add_action( 'admin_enqueue_scripts', array($this, 'enqueue'));
+
+            add_action( 'admin_menu', array($this, 'admin_control_menu') );
+
+            add_filter( "plugin_action_links_$this->plugin_link", array($this, 'plugin_dashborad_custom_link') );
+        }
+        public function plugin_dashborad_custom_link($links){
+            $new_link = '<a href="admin.php?page=new_plugin">Settings</a>';
+            array_push($links,$new_link);
+            return $links;
+        }
+        function admin_control_menu(){
+            add_menu_page( 'New Plugin Page', 'New Plugin', 'manage_options', 'new_plugin', array($this, 'admin_control_option'), 'dashicons-format-status', 101 );
+        }
+        function admin_control_option(){
+            echo '<h1>Welcome to New Plugin </h1>';
+            echo '<input type="color">';
         }
         function deactivation(){
             flush_rewrite_rules();
